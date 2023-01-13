e93c356bc744a078773178da122890dd51ef1a84
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-13 19:44:41 +0000

    psuedocode with instructions

---
+++ assets/questions.js
@@ -0,0 +1,7 @@
+// start button that includes timer 60 secs 
+// main question 
+// series of questions
+// variable to hold correct answers for highscore / adds 10 points
+// function that deducts timer by 10 secs if answer is incorrect
+// Navigate to next page once question is answered 
+// Feedback mechanism that alerts user if they are correct 
