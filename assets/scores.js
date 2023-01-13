6b49740b55fca3a57cfc2d2fd66c4ff3ba74fb64
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-13 19:49:46 +0000

    pseudocode for scores

---
+++ assets/scores.js
@@ -0,0 +1,5 @@
+// variable to tally score 
+// store tally with local storage to keep track of user's score
+// event listener for submit button for initials 
+// local storage for user's initials + score
+// The ability to clear the scores 
