adebc45430bb4b67a33f1e04b0dca47accf48346
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-13 20:05:01 +0000

    pseudocode to logic script

---
+++ assets/logic.js
@@ -0,0 +1,6 @@
+// if statements to validate user's answer 
+// feedback mechanism to alert user on if answer is correct or incorrect
+// variable to hold correct answers for highscore / adds 10 points
+// function that deducts timer by 10 secs if answer is incorrect
+// Navigate to next page once question is answered 
+// Feedback mechanism that alerts user if they are correct 
