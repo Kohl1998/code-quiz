90231f86796182e164c2b680a037b8838b8aa243
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-14 15:33:52 +0000

    transferred logic from questions script to logic script

--- assets/logic.js
+++ assets/logic.js
@@ -4,3 +4,22 @@
 // function that deducts timer by 10 secs if answer is incorrect
 // Navigate to next page once question is answered 
 // Feedback mechanism that alerts user if they are correct 
+
+var highscores = document.querySelector('.scores');
+var timer = document.querySelector('#time');
+var startButton = document.querySelector('.start');
+var revealQuestion = document.querySelector('#questions');
+var questionTitle = document.querySelector('#question-title');
+var question = document.querySelector('choices'); 
+var endScreen = document.querySelector('end-screen');
+var finalScore = document.querySelector('#final-score');
+var initials = document.querySelector('#initials'); 
+var feedback = document.querySelector('#feedback');
+
+var seconds = 60
+timer.textContent = seconds
+
+// create event listener for start button
+startButton.addEventListener("click",  setTime)
+
+// Create function for timer
