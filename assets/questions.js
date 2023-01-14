80f6c53ba638381a7e87a7e5d43322d7b2e6b5b7
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-14 14:58:16 +0000

    selected relative elements within html

--- assets/questions.js
+++ assets/questions.js
@@ -5,3 +5,16 @@
 // function that deducts timer by 10 secs if answer is incorrect
 // Navigate to next page once question is answered 
 // Feedback mechanism that alerts user if they are correct 
+// select all relevant elements
+
+var highscores = document.querySelector('.scores');
+var timer = document.querySelector('.timer');
+var startButton = document.querySelector('.start');
+var questionTitle = document.querySelector('#question-title');
+var question = document.querySelector('choices'); 
+var endScreen = document.querySelector('end-screen');
+var finalScore = document.querySelector('#final-score');
+var initials = document.querySelector('#initials'); 
+var feedback = document.querySelector('#feedback');
+
+console.log(finalScore);
