ec579715a8f6715cb74376d2407c07223308fda4
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-15 14:27:46 +0000

    created new array of objects with Q&A

--- assets/questions.js
+++ assets/questions.js
@@ -21,30 +21,34 @@ var feedback = document.querySelector('#feedback');
 console.log(finalScore);
 
 
-// append questions (1)
-
-questionTitle.textContent = "Who created Javascript?"
-
-// append series of questions (2)
-
-Option1 = document.createElement("button"); 
-Option1.textContent = "Brendan Eich"
-
-Option2 = document.createElement("button"); 
-Option2.textContent = "Bill Gates"
-
-Option3 = document.createElement("button"); 
-Option3.textContent = "Elon Musk"
-
-Option4 = document.createElement("button"); 
-Option4.textContent = "John Carmack"
-
-// Appends questions to div with Id of 'choices' 
-question.appendChild(Option1);
-question.appendChild(Option2);
-question.appendChild(Option3);
-question.appendChild(Option4);
-
-
-
-
+var Questions =  [{
+    Question1: "who created Javascript?" , 
+    choices: {
+        A: "Brendan Eich",
+        B: "Bill Gates",
+        C: "Elon Musk",
+        D: "John Doe"
+    }, 
+    Answer: 'A', 
+    Question2: "How do you write an alert message?", 
+    choices: {
+        A: "alert();", 
+        B: "confirm();",
+        C: "prompt();",
+        D: "function(){};",
+    }, Answer: 'A', 
+    Question3: "What does this sign mean ===", 
+    choices: {
+        A: "Stricly not equal too",
+        B: "Equal in value/type", 
+        C: "Greater than",
+        D: "Less than",
+    }, Answer: 'B', 
+    Question4: "How do you mean comments in HTML?",
+    choices: {
+        A: "//",
+        B: "/**/",
+        C: "<!--->",
+    }, Answer: 'C'
+} 
+]
